2b9d1b4e857e9810ebb56a8791fc3635f40eebbe
Parker Phillips <ExxCaelum@gmail.com>
2016-03-29 14:52:26 -0600

    Remove extraneous files

--- jungle_beats.rb
+++
@@ -1,2 +0,0 @@
-require './node'
-require './linkedlist'
--- node.rb
+++
@@ -1,9 +0,0 @@
-class Node
-  attr_accessor :data, :next_node
-
-  def initialize(data = nil, next_node = nil)
-    @data = data
-    @next_node = next_node
-  end
-
-end
